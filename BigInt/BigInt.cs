ca062eb3d81687c075f9be82eccd962bec7ba2d2
Lunat1c <arturgrabowski100@onet.pl>
2022-10-24 09:13:36 +0200

    Add project files.

---
+++ BigInt/BigInt.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigInt
+{
+    internal class BigInt
+    {
+        #region Node subclass
+
+        #endregion
+
+
+        #region Properties
+
+        #endregion
+
+
+        #region Constructor
+        public BigInt(string value)
+        {
+
+        }
+
+        public BigInt()
+        {
+            
+        }
+        #endregion
+
+
+        #region Methods
+        private void AddDigitInFront(int digit)
+        {
+            throw new NotImplementedException();
+        }
+
+        override public string ToString()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
---
+++ BigInt/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace BigInt
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Benc benc");
+        }
+    }
+}
